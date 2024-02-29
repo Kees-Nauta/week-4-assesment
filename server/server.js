3070f79ce3ffe67da2ae66a6d51b031704a562fb
Kees Nauta <nautainseacula.com>
2024-02-29 14:35:34 -0700

    commit 4

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 
-app.use(express.static(`${__dirname}/client`))
+app.use(express.static(`${__dirname}/../client`))
 
 var Rollbar = require('rollbar')
 var rollbar = new Rollbar({
